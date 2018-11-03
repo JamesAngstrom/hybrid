4c5f83a714ac2f6d6259adca0f1c6e4d644c557c
James Angstrom <james.angstrom@gmail.com>
2018-11-03 02:14:53 +0000

    Implement Bicubic Patches for procedural worldgen

--- src/hybrid.rs
+++ src/hybrid.rs
@@ -4,7 +4,7 @@ use amethyst::{
     core::Transform,
     core::cgmath::{Vector3, Deg},
     assets::{Loader},
-    renderer::{Rgba, Projection, Camera, Material, MaterialDefaults, ObjFormat, Light, PointLight},
+    renderer::{MeshHandle, Rgba, Projection, PosNormTex, Camera, Material, MaterialDefaults, ObjFormat, Light, PointLight},
 };
 
 use gilrs::Event;
@@ -17,6 +17,96 @@ impl Component for Ball {
     type Storage = DenseVecStorage<Self>;
 }
 
+struct BicubicPatch {
+    controls: [[Vector3<f32>; 4]; 4]
+}
+
+
+fn fac(n: i32) -> i32 {
+    assert!(n >= 0);
+
+    let mut m = 1; 
+    for i in 1..(n + 1) {
+        m = m * i
+    };
+    m
+}
+
+fn binomial3(i: i32) -> f32 {
+    6.0 / (fac(i) * fac(3 - i)) as f32
+}
+
+fn bernstein3(i: i32, u: f32) -> f32 {
+    binomial3(i) * u.powi(i) * (1.0 - u).powi(3 - i)
+}
+
+impl BicubicPatch {
+    pub fn new() -> Self {
+        let v0  = Vector3::new(-0.3, -0.3, 0.0);
+        let v1  = Vector3::new(0.0, 0.2, 0.1);
+        let v2  = Vector3::new(0.0, 0.4, 0.1);
+        let v3  = Vector3::new(0.0, 0.6, 0.0);
+        let v4  = Vector3::new(0.2, 0.0, 0.0);
+        let v5  = Vector3::new(0.2, 0.2, 0.0);
+        let v6  = Vector3::new(0.2, 0.4, 0.0);
+        let v7  = Vector3::new(0.2, 0.6, 0.0);
+        let v8  = Vector3::new(0.4, 0.0, 0.0);
+        let v9  = Vector3::new(0.4, -0.4, 0.0);
+        let v10 = Vector3::new(0.4, 1.4, 0.9);
+        let v11 = Vector3::new(0.4, 0.6, 0.0);
+        let v12 = Vector3::new(0.6, 0.0, 0.0);
+        let v13 = Vector3::new(0.6, 0.2, 0.0);
+        let v14 = Vector3::new(0.6, 0.4, 0.0);
+        let v15 = Vector3::new(0.6, 0.6, 0.0);
+
+        BicubicPatch { controls: [[v0,  v1,  v2,  v3],
+                                  [v4,  v5,  v6,  v7],
+                                  [v8,  v9,  v10, v11],
+                                  [v12, v13, v14, v15]] }
+    }
+    
+    fn control(&self, i: i32, j: i32) -> Vector3<f32> {
+        self.controls[i as usize][j as usize]
+    }
+    
+    fn pos(&self, u: f32, v: f32) -> Vector3<f32> {
+        // Only defined for the unit square
+        assert!(0.0 <= u && u <= 1.0 && 0.0 <= v && v <= 1.0);
+        
+        let mut sum = Vector3::new(0.0, 0.0, 0.0);
+        for i in 0..4 {
+            for j in 0..4 {
+                sum += (bernstein3(i, u) * bernstein3(j, v)) * self.control(i, j)
+            }
+        };
+        sum
+    }
+
+    fn rasterize(&self, res: i32) -> Vec<PosNormTex> {
+        let mut vec = Vec::new();
+        for row in 0..res {
+            for col in 0..res {
+                for (r, c) in [(0.0, 0.0), (0.0, 1.0), (1.0, 1.0)].iter() {
+                    let u = (row as f32 + r) / res as f32;
+                    let v = (col as f32 + c) / res as f32;
+                    let p = self.pos(u, v);
+                    vec.push(PosNormTex {
+                        position: p.into(),
+                        normal: [0.0, 0.0, 0.1],
+                        tex_coord: [u, v]
+                    })
+                }
+            }
+        };
+        vec
+    }
+}
+
+fn create_mesh(world: &World, vertices: Vec<PosNormTex>) -> MeshHandle {
+    let loader = world.read_resource::<Loader>();
+    loader.load_from_data(vertices.into(), (), &world.read_resource())
+}
+
 pub struct Hybrid;
 
 impl<'a, 'b> State<GameData<'a, 'b>, Event> for Hybrid {
@@ -27,11 +117,11 @@ impl<'a, 'b> State<GameData<'a, 'b>, Event> for Hybrid {
         initialize_lights(world);
 
         let (mesh, mtl) = {
-            let mat_defaults = world.read_resource::<MaterialDefaults>();
-            let loader = world.read_resource::<Loader>();
-
             let meshes = &world.read_resource();
             let textures = &world.read_resource();
+
+            let loader = world.read_resource::<Loader>();
+            let mat_defaults = world.read_resource::<MaterialDefaults>();
 
             let mesh = loader.load("mesh/teapot.obj", ObjFormat, (), (), meshes);
             let albedo = loader.load_from_data([0.0, 0.0, 1.0, 0.0].into(), (), textures);
@@ -54,11 +144,36 @@ impl<'a, 'b> State<GameData<'a, 'b>, Event> for Hybrid {
         world
             .create_entity()
             .with(mesh)
-            .with(mtl)
+            .with(mtl.clone())
             .with(trans)
             .with(Ball {
                 velocity: [0.0, 0.0]
             })
+            .build();
+
+        let patch = BicubicPatch::new();
+        let mesh = create_mesh(world, patch.rasterize(32));
+
+        let mtl = {
+            let loader = world.read_resource::<Loader>();
+            let mat_defaults = world.read_resource::<MaterialDefaults>();
+
+            let albedo = loader.load_from_data([1.0, 0.0, 0.0, 0.0].into(), (), &world.read_resource());
+
+            Material {
+                albedo,
+                ..mat_defaults.0.clone()
+            }
+        };
+
+        let mut trans = Transform::default();
+        trans.scale = Vector3::new(20.0, 20.0, 20.0);
+
+        world
+            .create_entity()
+            .with(mesh)
+            .with(mtl)
+            .with(trans)
             .build();
 
     }
